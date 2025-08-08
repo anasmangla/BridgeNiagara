5d8ec85a7a0ff3870437ecd69cc81a9142e6a364
anasonix <anas.mangla@gmail.com>
2025-08-08 13:29:21 -0400

    Add Passenger WSGI entrypoint for Django

---
+++ passenger_wsgi.py
@@ -0,0 +1,5 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bridgeniagara.settings')
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
